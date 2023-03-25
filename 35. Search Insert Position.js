8a4084dd69b568dcd18ae848381adb500456f5a0
Pilon245 <112905097+Pilon245@users.noreply.github.com>
2023-03-25 14:02:34 +0300

    Add files via upload

---
+++ 35. Search Insert Position.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const searchInsert = function(nums, target) {
+    for (let i = 0; i < nums.length; i++){
+        // if(nums[nums.length/2] > )
+    }
+};
+
+const nums = [0,0,1,1,1,2,2,3,3,4]
+const target = 3
+console.log(searchInsert(nums, target))
+
+/*
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
